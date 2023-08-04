83091f85a8f5776d20c5fe3e524d824f2d77d19b
xming-ubuntu <wsm_xming@163.com>
2023-08-05 00:59:53 +0800

    update: Multithreaded concurrent server and client

---
+++ TCP_Learning/tcp_in_linux/src/chat_clint.c
@@ -0,0 +1,109 @@
+#include <netinet/in.h>
+#include <stddef.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <pthread.h>
+#include <sys/io.h>
+#include <string.h>
+#include <sys/types.h>
+#include <unistd.h>
+
+#define BUF_SIZE 1024
+#define NAME_SIZE 30
+
+void* send_msg(void* arg);
+void* read_msg(void* arg);
+void error_msg(char* msg);
+
+char name[NAME_SIZE] = "[DEFAULT]";
+char msg[BUF_SIZE];
+
+int main(int argc, char *argv[])
+{
+  int sock;
+  struct sockaddr_in serv_adr;
+  pthread_t send_thread, read_thread;
+  void* thread_return;
+  if (argc != 4) {
+    printf("Usage: %s <IP> <Port> <Name>\n", argv[0]);
+    exit(1);
+  }
+
+  sprintf(name, "[%s]", argv[3]);
+
+  sock = socket(PF_INET, SOCK_STREAM, 0);
+  memset(&serv_adr, 0, sizeof(serv_adr));
+  serv_adr.sin_family = AF_INET;
+  serv_adr.sin_addr.s_addr = inet_addr(argv[1]);
+  serv_adr.sin_port = htons(atoi(argv[2]));
+
+  if (connect(sock, (struct sockaddr *)&serv_adr, sizeof(serv_adr)) == -1) {
+    error_msg("connect() error");
+  }
+
+  pthread_create(&send_thread, NULL, send_msg, (void*)&sock);
+  pthread_create(&read_thread, NULL, read_msg, (void*)&sock);
+  pthread_join(send_thread, &thread_return);
+  pthread_join(read_thread, &thread_return);
+
+  close(sock);
+
+  return EXIT_SUCCESS;
+}
+
+void* send_msg(void* arg) {
+  int sock = *((int*)arg);
+  char name_msg[NAME_SIZE + BUF_SIZE];
+  while (1) {
+    fgets(msg, BUF_SIZE, stdin);
+    if (!strcmp(msg, "q\n") || !strcmp(msg, "Q\n")) {
+      close(sock);
+      exit(1);
+    }
+    sprintf(name_msg, "%s %s", name, msg);
+    write(sock, name_msg, strlen(name_msg));
+  }
+  return NULL;
+}
+
+void* read_msg(void* arg) {
+  int sock = *((int*)arg);
+  char name_msg[NAME_SIZE + BUF_SIZE];
+  int str_len;
+  while (1) {
+    str_len = read(sock, name_msg, NAME_SIZE + BUF_SIZE);
+    if (str_len == -1) {
+      return (void*)-1;
+    }
+    name_msg[str_len] = '\0';
+    fputs(name_msg, stdout);
+  }
+  return NULL;
+}
+
+void error_msg(char* msg) {
+  fputs(msg, stderr);
+  fputc('\n', stderr);
+  exit(1);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- TCP_Learning/tcp_in_linux/src/chat_server.c
+++ TCP_Learning/tcp_in_linux/src/chat_server.c
@@ -1,3 +1,5 @@
+#include <netinet/in.h>
+#include <stddef.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <sys/socket.h>
@@ -23,6 +25,46 @@ pthread_mutex_t mutx;
 
 int main(int argc, char *argv[])
 {
+  int serv_sock, clnt_sock;
+  struct sockaddr_in serv_adr, clnt_adr;
+  socklen_t clnt_adr_sz;
+  pthread_t t_id;
+
+  if (argc != 2) {
+    printf("Usage: %s <port>\n", argv[0]);
+    exit(1);
+  }
+
+  pthread_mutex_init(&mutx, NULL);
+  serv_sock = socket(PF_INET, SOCK_STREAM, 0);
+
+  int flag = 1;
+  setsockopt(serv_sock, SOL_SOCKET, SO_REUSEPORT, &flag, sizeof(flag));
+  memset(&serv_adr, 0, sizeof(serv_adr));
+  serv_adr.sin_family = AF_INET;
+  serv_adr.sin_addr.s_addr = htonl(INADDR_ANY);
+  serv_adr.sin_port = htons(atoi(argv[1]));
+
+  if (bind(serv_sock, (struct sockaddr *)&serv_adr, sizeof(serv_adr)) == -1) {
+    error_msg("bind() error");
+  }
+  if (listen(serv_sock, 5) == -1) {
+    error_msg("listen() error");
+  }
+
+  while (1) {
+    clnt_adr_sz = sizeof(clnt_adr);
+    clnt_sock = accept(serv_sock, (struct sockaddr *)&clnt_adr, &clnt_adr_sz);
+
+    pthread_mutex_lock(&mutx);
+    clnt_socks[clnt_cnt ++] = clnt_sock;
+    pthread_mutex_unlock(&mutx);
+
+    pthread_create(&t_id, NULL, handle_clnt, (void*)&clnt_sock);
+    pthread_detach(t_id);
+    printf("Connected client IP: %s\n", inet_ntoa(clnt_adr.sin_addr));
+  }
+
 
   return EXIT_SUCCESS;
 }
@@ -61,8 +103,9 @@ void* handle_clnt(void* arg) {
 void send_msg(char* msg, int len) {
   pthread_mutex_lock(&mutx);
   for (int i = 0; i < clnt_cnt; i ++) {
-
+    write(clnt_socks[i], msg, len);
   }
+  pthread_mutex_unlock(&mutx);
 }
 
 
